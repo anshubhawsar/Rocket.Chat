b5952313e86ba57856a7fdde7c0fa94f8fca93b3
Rodrigo Nascimento <rodrigoknascimento@gmail.com>
2026-03-07 15:27:29 -0300

    Add migration to disable receipts archive if existent dataset is big

--- apps/meteor/jest.config.ts
+++ apps/meteor/jest.config.ts
@@ -43,6 +43,7 @@ export default {
 				'<rootDir>/app/cloud/server/functions/supportedVersionsToken/**.spec.ts',
 				'<rootDir>/app/utils/lib/**.spec.ts',
 				'<rootDir>/server/lib/auditServerEvents/**.spec.ts',
+				'<rootDir>/server/lib/dataMigrations.spec.ts',
 				'<rootDir>/server/services/import/**/*.spec.ts',
 				'<rootDir>/server/settings/lib/**.spec.ts',
 				'<rootDir>/server/cron/**.spec.ts',
